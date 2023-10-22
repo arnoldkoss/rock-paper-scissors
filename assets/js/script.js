f7f2a8d9b8a913b75209daff7c631b66132ae848
arnoldkoss <>
2023-10-22 15:27:21 +0000

    set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = this.getElementsByTagName("button");
+
+    for (let bu)
+}
+
+
+
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function incrementPlayerScore(){
+
+}
+
+function incrementComputerScore(){
+    
+}
