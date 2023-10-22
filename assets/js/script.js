75d580f588b111cd459228e1055f160e2e5d8fc0
arnoldkoss <>
2023-10-22 15:59:10 +0000

    add event listeners to the buttons and display alert when clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,20 @@
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
 
-document.addEventListener("DOMContentLoaded", function(){
-    let buttons = this.getElementsByTagName("button");
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
 
-    for (let bu)
-}
-
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-choice") === "rock") {
+                alert("you clicked rock");
+            } else {
+                let buttonChoice = this.getAttribute("data-choice");
+                alert(`You clicked ${buttonChoice}`);
+            }
+        });
+    }
+});
 
 
 function runGame(){
@@ -22,5 +30,5 @@ function incrementPlayerScore(){
 }
 
 function incrementComputerScore(){
-    
+
 }
